89fe89ff4941ef34b1cdd457d18e3023a63b8e7f
tceseenu <151898741+tceseenu@users.noreply.github.com>
2023-12-12 16:55:45 +0530

    Update AjioAssignment.java

--- AjioAssignment.java
+++ AjioAssignment.java
@@ -1,4 +1,4 @@
-package Week4.day1;
+package week4.day1;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -13,37 +13,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AjioAssignment {
 
 	public static void main(String[] args) throws InterruptedException {
-			// TODO Auto-generated method stub
-			ChromeDriver driver=new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.get(" https://www.ajio.com/");
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-			driver.findElement(By.xpath("//input[@placeholder='Search AJIO']")).sendKeys("bags",Keys.ENTER);
-			driver.findElement(By.xpath("(//label[contains(@class,'facet-linkname-Men')])[1]")).click();
-			 boolean sel = driver.findElement(By.xpath("//label[contains(text(),'Fashion Bags')]")).isEnabled();
-			 System.out.println(sel);
-			// driver.findElement(By.xpath("//label[contains(text(),'Fashion Bags')]")).click();
-			//String text = driver.findElement(By.xpath("//strong[text()='3,572']")).getText();
-			 //System.out.println(text);
-			 List<WebElement> count = driver.findElements(By.xpath("(//div[@class='brand']/strong)"));
-			 List<String> brands=new ArrayList<String>();
-			 for(int i=0;i<count.size();i++) {
-				 String brandName=count.get(i).getText();
-				 //System.out.println(brandName);
-				 brands.add(brandName);
-			 }
-			 //System.out.println(brands);
-			 Collections.sort(brands);
-			 System.out.println(brands);
-			 
-			 List<WebElement> bags = driver.findElements(By.xpath("//div[@class='nameCls']"));
-			 List<String> bagNames=new ArrayList<String>();
-			 for(int j=0;j<bags.size();j++) {
-				 String bagNa=bags.get(j).getText();
-				 bagNames.add(bagNa);
-			 }
-			// System.out.println(bagNames);
-			 Collections.sort(bagNames);
-			 System.out.println(bagNames);
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(" https://www.ajio.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("//input[@placeholder='Search AJIO']")).sendKeys("bags", Keys.ENTER);
+		driver.findElement(By.xpath("(//label[contains(@class,'facet-linkname-Men')])[1]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//label[@for='Men - Fashion Bags']")).click();
+		//Total Count of Bags
+				Thread.sleep(3000);
+				String text = driver.findElement(By.xpath("//div[@class='length']/strong")).getText();
+				System.out.println(text);
+		List<WebElement> count = driver.findElements(By.xpath("(//div[@class='brand']/strong)"));
+		List<String> brands = new ArrayList<String>();
+		for (int i = 0; i < count.size(); i++) {
+			String brandName = count.get(i).getText();
+			// System.out.println(brandName);
+			brands.add(brandName);
 		}
+		// System.out.println(brands);
+		Collections.sort(brands);
+		System.out.println(brands);
+
+		List<WebElement> bags = driver.findElements(By.xpath("//div[@class='nameCls']"));
+		List<String> bagNames = new ArrayList<String>();
+		for (int j = 0; j < bags.size(); j++) {
+			String bagNa = bags.get(j).getText();
+			bagNames.add(bagNa);
+		}
+		// System.out.println(bagNames);
+		Collections.sort(bagNames);
+		System.out.println(bagNames);
 	}
+}
