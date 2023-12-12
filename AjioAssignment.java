e16619db879d69bc2ff0386e260e40a89531ed48
tceseenu <151898741+tceseenu@users.noreply.github.com>
2023-12-12 09:29:52 +0530

    Add files via upload

---
+++ AjioAssignment.java
@@ -0,0 +1,49 @@
+package Week4.day1;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AjioAssignment {
+
+	public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+			ChromeDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get(" https://www.ajio.com/");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.findElement(By.xpath("//input[@placeholder='Search AJIO']")).sendKeys("bags",Keys.ENTER);
+			driver.findElement(By.xpath("(//label[contains(@class,'facet-linkname-Men')])[1]")).click();
+			 boolean sel = driver.findElement(By.xpath("//label[contains(text(),'Fashion Bags')]")).isEnabled();
+			 System.out.println(sel);
+			// driver.findElement(By.xpath("//label[contains(text(),'Fashion Bags')]")).click();
+			//String text = driver.findElement(By.xpath("//strong[text()='3,572']")).getText();
+			 //System.out.println(text);
+			 List<WebElement> count = driver.findElements(By.xpath("(//div[@class='brand']/strong)"));
+			 List<String> brands=new ArrayList<String>();
+			 for(int i=0;i<count.size();i++) {
+				 String brandName=count.get(i).getText();
+				 //System.out.println(brandName);
+				 brands.add(brandName);
+			 }
+			 //System.out.println(brands);
+			 Collections.sort(brands);
+			 System.out.println(brands);
+			 
+			 List<WebElement> bags = driver.findElements(By.xpath("//div[@class='nameCls']"));
+			 List<String> bagNames=new ArrayList<String>();
+			 for(int j=0;j<bags.size();j++) {
+				 String bagNa=bags.get(j).getText();
+				 bagNames.add(bagNa);
+			 }
+			// System.out.println(bagNames);
+			 Collections.sort(bagNames);
+			 System.out.println(bagNames);
+		}
+	}
